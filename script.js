7fd0c6a69633aa05eee69221a8a392ef85a67bdf
jackandking <jackandking@users.noreply.github.com>
2025-10-16 12:13:40 +0000

    deploy: jackandking/MuseumCheck@36d28dfb24ac39b724ca1320e0829d8755bcf7f9

--- script.js
+++ script.js
@@ -4958,7 +4958,7 @@ class MuseumCheckApp {
                             <h3>
                                 ${museum.name}
                                 <button class="museum-fireworks-button" data-museum="${museum.id}" title="查看本馆烟花墙" style="display: none;">🎆</button>
-                                ${museum.id === 'forbidden-city' ? '<button class="museum-checkin-button" data-museum="' + museum.id + '" title="进入打卡页面">🔗 打卡</button>' : ''}
+                                ${(museum.id === 'forbidden-city' || museum.id === 'zhaoyuan-hengli-watch-museum') ? '<button class="museum-checkin-button" data-museum="' + museum.id + '" title="进入打卡页面">🔗 打卡</button>' : ''}
                                 ${isVisited && !this.assessmentHidden ? '<button class="assessment-button" data-museum="' + museum.id + '" title="亲子关系测评">🧡 亲子测评</button>' : ''}
                             </h3>
                             <div class="museum-location">📍 ${museum.location}</div>
